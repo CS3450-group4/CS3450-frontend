83b16f8c109928405873902efb7ffa72df7e8acb
Jacob Thompson <a01657923@usu.edu>
2022-09-14 18:00:27 -0600

    revert rebase experiment

--- api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from base.models import MenuItem
-
-class MenuItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MenuItem
-        fields = '__all__'
-
-
