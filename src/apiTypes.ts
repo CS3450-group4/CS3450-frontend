0bbcdc16f3743a0d4d40fff407fbc50accaa5e82
Jacob Thompson <a01657923@usu.edu>
2022-10-10 12:52:10 -0600

    minor change to types

--- src/apiTypes.ts
+++ src/apiTypes.ts
@@ -1,4 +1,5 @@
 export type User = {
+  readonly id: number
   authLevel: [number]
   password: string
   userName: string
@@ -26,8 +27,21 @@ export type Ingredient = {
 
 export type Order = {
   price: number
-  time: string
-  user: string
+  created: string
+  user: number
   orderStatus: "unfullfiled" | "fullfilled" | "canceled"
   ingredientsList: [string]
 }
+/*  example usage
+const exampleOrder: Order = {
+created: 2022-09-12T04:37:28.004073Z,
+price: 200,
+user: someUserInstance.id,
+orderStatus: "unfullfiled",
+ingredientsList: ["coffee", "milk", "someotherspice"]
+}
+
+sidenote: dont worry too much about the date format
+I will make a utility to convery js date objects into
+the api format and vis versa
+*/
