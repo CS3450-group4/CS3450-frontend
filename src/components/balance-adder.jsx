228c9a43d1a6d5a2e58f935c40457f0537d45190
Isaac Airmet <a02333866@usu.edu>
2022-10-12 20:38:59 -0600

    Backend links to balance and hours adding

--- src/components/balance-adder.jsx
+++ src/components/balance-adder.jsx
@@ -1,14 +1,30 @@
-import {useState} from "react";
+import {useState, useEffect } from "react";
 import {
     Stack,
     Box,
     TextField,
     Button,
   } from "@mui/material";
-
+import user from "./tempObject"
 export default function BalanceAdder(props) {
     const [balance, setBalance] = useState(0);
     const [isInvalidInput, setIsInvalidInput] = useState(false);
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    function fetchData() {
+        fetch(`http://localhost:8000/api/user/${user.id}/`)
+        .then((res) => res.json())
+        .then(
+          (data) => {
+              setBalance(data.balance);
+              setCurrentUser(data)
+          }
+        )
+    }
 
     function updateBalance(event) {
         try {
@@ -16,20 +32,30 @@ export default function BalanceAdder(props) {
             const inputedData = Number(event.target.value);
             if (Number.isInteger(inputedData)) {
                 setBalance(event.target.value);
+
             } else {
                 setIsInvalidInput(true)
             }
         } catch (error) {
-            alert(error)
+            console.log(error)
         }
     }
 
     function submitBalance() {
         try {
-            // TODO: API call to submit hours
+            currentUser.balance = balance;
+            fetch(`http://localhost:8000/api/user/${user.id}/`, {
+                method: 'PUT',
+                mode: 'cors',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                'body': JSON.stringify(currentUser),
+              })
         } catch (error) {
-            
+            console.log(error);
         }
+        fetchData();
     }
 
     return (
