aca3536209eda8558f933e425912a1c3a77279a9
Jacob Thompson <a01657923@usu.edu>
2022-10-24 12:00:15 -0600

    reformatted login page and added token based authenitcation

--- src/components/login-signup.jsx
+++ src/components/login-signup.jsx
@@ -3,66 +3,79 @@ import { useNavigate } from "react-router-dom";
 import "./login-signup.css";
 import "./../App.css";
 import {
-    Box,
-    Button,
-    Card,
-    TextField,
-    Stack
-  } from "@mui/material";
+  Box,
+  Button,
+  Card,
+  TextField,
+  Stack
+} from "@mui/material";
 import TitleHeader from "./header";
 
 export default function Login() {
-    const [userName, setUserName] = useState(null);
-    const [password, setPassword] = useState(null);
-    let navigate = useNavigate();
-    const viewStrings = ["customer", "cashier", "barista", "manager"];
+  const [userName, setUserName] = useState(null);
+  const [password, setPassword] = useState(null);
+  let navigate = useNavigate();
+  const viewStrings = ["customer", "cashier", "barista", "manager"];
 
-    function updatePassword(event) {
-        setPassword(event.target.value);
-    }
+  function updatePassword(event) {
+    setPassword(event.target.value);
+  }
 
-    function updateUserName(event) {
-        setUserName(event.target.value);
-    }
+  function updateUserName(event) {
+    setUserName(event.target.value);
+  }
 
-    function logIn() {
-        // TODO: Verify user login
-        navigate(viewStrings[2]);
-    }
+  async function logIn() {
 
-    function signUp() {
-        // TODO: Verify user signup
-        navigate(viewStrings[1]);
-    }
+    await fetch('http://localhost:8000/api/api-token-auth/', {
 
-    return (
-        <div className="App">
-            <Box className="TitleHeaderContainer">
-                <TitleHeader />
-            </Box>
-            
-            <Box class="container">
-                <Card sx={{padding: '16px'}}>
-                    <Stack direction="column" spacing={2}>
-                        <TextField 
-                            variant="standard" 
-                            label="UserName" 
-                            value={userName} 
-                            onChange={updateUserName}
-                        />
-                        <TextField 
-                            variant="standard" 
-                            label="Password" 
-                            value={password} 
-                            onChange={updatePassword}
-                        />
-                    </Stack>
-                    <Stack direction="row" spacing={2}>
-                        <Button variant="Contained" onClick={() => logIn()}>Login</Button>
-                        <Button variant="Outlined" onClick={() => signUp()}>Sign Up</Button>
-                    </Stack>
-                </Card>
-            </Box>
-        </div>
-    )
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      'body': JSON.stringify({
+        username: userName,
+        password: password
+      }),
+    }).then((res) => { if (res.ok) navigate(viewStrings[2]) })
+      .catch((err) => console.log(err))
+
+  }
+
+  function signUp() {
+    // TODO: Verify user signup
+    navigate(viewStrings[1]);
+  }
+
+  return (
+    <div className="App">
+      <Box className="TitleHeaderContainer">
+        <TitleHeader />
+      </Box>
+
+      <Box class="container">
+        <Card sx={{ padding: '16px' }}>
+          <Stack direction="column" spacing={2}>
+            <TextField
+              variant="standard"
+              label="UserName"
+              value={userName}
+              onChange={updateUserName}
+            />
+            <TextField
+              variant="standard"
+              label="Password"
+              value={password}
+              onChange={updatePassword}
+            />
+          </Stack>
+          <Stack direction="row" spacing={2}>
+            <Button variant="Contained" onClick={() => logIn()}>Login</Button>
+            <Button variant="Outlined" onClick={() => signUp()}>Sign Up</Button>
+          </Stack>
+        </Card>
+      </Box>
+    </div>
+  )
 }
