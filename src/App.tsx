c17e8ab7986f9dbc3e0aefc861ebf7056b6fb063
Zach Jordan <a02310139@usu.edu>
2022-10-25 18:52:02 -0600

    merge conflict resolutiong

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,7 @@ import Login from './components/login-signup';
 
 function App() {
 
-  // const [apiData, setApiData] = useState()
-  // useEffect(() => {
-  //     fetch('http://localhost:8000/api/')
-  //       .then((res) => res.json())
-  //       .then((data) => setApiData(data))
-  // }, [])
+  const [apiData, setApiData] = useState()
   
   return (
     <BrowserRouter>
