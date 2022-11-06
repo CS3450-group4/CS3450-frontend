80343f6ed5bd7b1f2e2e161ccf0e6a4393cfba5c
jason-jackson6 <jcjackson02@gmail.com>
2022-11-06 16:22:09 -0700

    Fixed stuff.

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,21 @@ import BaristaView from './components/main-view-components/barista-view';
 import CashierView from './components/main-view-components/cashier-view';
 import ManagerView from './components/main-view-components/manager-view';
 import CustomerView from './components/main-view-components/customer-view';
-import TestApi from './components/test-api';
 import Login from './components/login-signup';
 
 
 function App() {
-
-  const [apiData, setApiData] = useState()
-  
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Login />} />
         <Route path="customer/*" element={<CustomerView />} />
-        <Route path="cashier" element={<CashierView />} />
-        <Route path="barista/*" element={<BaristaView />} />
+        <Route path="cashier/*" element={<CashierView />} />
+        <Route path="barista" element={<BaristaView />} />
         <Route path="manager/*" element={<ManagerView />} />
       </Routes>
     </BrowserRouter>
 
-
-
-
-    // <div className="App">
-    //   <Login />
-    // </div>
   );
 }
 
