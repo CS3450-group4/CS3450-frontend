448ff361f69c13872a29ca84cbe2ad1b1b6f1acb
Jacob Thompson <a01657923@usu.edu>
2022-11-14 19:18:13 -0700

    Fix barista view

--- src/components/barista-components/check-ingredients.jsx
+++ src/components/barista-components/check-ingredients.jsx
@@ -1,130 +1,119 @@
-import {useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 import {
-    Stack,
-    Box,
-    TextField,
-    Button,
-    FormControl,
-    Checkbox,
-    Typography,
-    FormControlLabel,
-    MenuItem,
-    InputLabel,
-    Select,
-    GridItem,
-    List
-  } from "@mui/material";
+  Stack,
+  Box,
+  Button,
+  FormControl,
+  Checkbox,
+  FormControlLabel,
+  CircularProgress,
+} from '@mui/material'
 export default function AddDrink(props) {
-    const [ingredients, setIngredients] = useState([]);
-    const [currentDrink, setCurrentDrink] = useState(null);
-    const [isGrabbed, setIsGrabbed] = useState([]);
-    const [disableMakeDrink, setDisableMakeDrink] = useState(true);
-    let itemsGrabbedCount = 0;
-    useEffect(() => {
-        fetchOrderData();
-    }, [])
+  const [orders, setorders] = useState([])
+  const [isOrdersLoading, setOrdersLoading] = useState(false)
+  const [ingredients, setIngredients] = useState([])
+  const [currentDrink, setCurrentDrink] = useState(null)
+  const [isGrabbed, setIsGrabbed] = useState([])
+  const [disableMakeDrink, setDisableMakeDrink] = useState(true)
+  useEffect(() => {
+    fetchOrderData()
+  }, [])
 
-
-    function fetchOrderData() {
-        fetch('http://localhost:8000/api/orders/')
-        .then((res) => res.json())
-        .then(
-            (data) => {
-                console.log("GRABBING");
-                var tempList = []
-                data.forEach(order => {
-                    if (order.orderStatus === "readyToFullfill") {
-                        tempList.push(order)
-                    }
-                })
-                console.log(tempList);
-                if (tempList.length == 0) return;
-                setCurrentDrink(tempList[0]);
-                buildButtons(tempList[0]);
-            }
-        )
-    }
-
-    function checkIngrident(event, index, drink) {
-        isGrabbed[index] = event.target.checked;
-        event.target.disabled=true;
-        setIsGrabbed(isGrabbed);
-        console.log(isGrabbed);
-        itemsGrabbedCount++;
-        console.log("BELOW")
-        console.log(Object.entries(drink.ingredientList)[0])
-        if (itemsGrabbedCount == Object.entries(drink.ingredientList)[0][1].length) {
-            setDisableMakeDrink(false);
-        }
-    }
-
-    function buildButtons(drink) {
-        setIsGrabbed([]);
-        setIngredients(Object.entries(drink.ingredientList)[0][1].map((ingrident, index) =>
-            <FormControlLabel key={index} control={<Checkbox  checked={isGrabbed[index]} onChange={(newVal) => checkIngrident(newVal, index, drink)} /> } label={ingrident["name"]} />
-        ))
-    }
-
-    async function buildDrink() {
-        // Get all the ingridents of the current drink
-        // Remove ingridents from inventory+
-        // Change status of order
-        // Reset isGrabbed, currentDrink, disableMakeDrink, ingridents
-        console.log("HERE")
-        console.log(currentDrink)
-        await Object.entries(currentDrink.ingredientList)[0][1].forEach(ingredient => {
-            console.log(Object.entries(currentDrink.ingredientList)[0][1])
-            // console.log(Object.entries(currentDrink.ingredientList)[1][1])
-            ingredient.stock -= 
-            fetch(`http://localhost:8000/api/ingredient/${ingredient.id}/`, {
-                method: 'PUT',
-                mode: 'cors',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                'body': JSON.stringify(ingredient),
-                }).then((res) => res.json())
-                .then((data) => {
-                    currentDrink.orderStatus = "forPickup"
-                })
+  function fetchOrderData() {
+    setOrdersLoading(true)
+    fetch('http://localhost:8000/api/orders/')
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        setOrdersLoading(false)
+        const ordersQueue = []
+        data.forEach((order) => {
+          if (order.orderStatus === 'readyToFullfill') {
+            ordersQueue.push(order)
+          }
         })
-        console.log("DONE WITH INGRED")
-        await fetch(`http://localhost:8000/api/orders/${currentDrink.id}/`, {
-            method: 'PUT',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            'body': JSON.stringify(currentDrink),
-            }).then((res) => res.json())
-            .then((data) => {
-                setCurrentDrink(null);
-                setIsGrabbed([]);
-                setDisableMakeDrink(true);
-                setIngredients([]);
-                console.log("BEFORE END");
-            }) 
-        await resetDynamicData()
-        console.log("END");
-        setTimeout(fetchOrderData(), 1000);
-    }
+        console.log(ordersQueue)
+        if (ordersQueue.length == 0) return
+        setorders(ordersQueue)
+        setCurrentDrink(ordersQueue[0])
+        buildButtons(ordersQueue[0])
+      })
+  }
 
-    async function resetDynamicData() {
-        setCurrentDrink(null);
-        setIsGrabbed([]);
-        setDisableMakeDrink(true);
-        setIngredients([]);
-        // buildButtons(currentDrink);
+  if (isOrdersLoading) {
+    return <CircularProgress />
+  }
+
+  function checkIngrident(event, index, drink) {
+    setIsGrabbed(() => {
+      const previousState = isGrabbed
+      previousState[index] = event.target.checked
+      return previousState
+    })
+    event.target.disabled = true
+    if (
+      isGrabbed.length == Object.entries(drink.ingredientList)[0][1].length
+    ) {
+      setDisableMakeDrink(false)
     }
-    
-    return ( 
-        <Box>
-            <Stack>
-                <FormControl className={props.ingredientClassName}>
-                    {ingredients}
-                </FormControl>
-                <Button disabled={disableMakeDrink} onClick={() => buildDrink()}>Make Drink</Button>
-            </Stack>
-        </Box> 
+  }
+
+  function buildButtons(drink) {
+    setIsGrabbed([])
+    setIngredients(
+      Object.entries(drink.ingredientList)[0][1].map((ingrident, index) => (
+        <FormControlLabel
+          key={index}
+          control={
+            <Checkbox
+              checked={isGrabbed[index]}
+              onChange={(newVal) => checkIngrident(newVal, index, drink)}
+            />
+          }
+          label={ingrident.name}
+        />
+      )),
     )
+  }
+
+  async function buildDrink() {
+    await fetch(`http://localhost:8000/api/fullfillorder/${currentDrink.id}/`, {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+        "Authorization": "Token " + window.localStorage.getItem('token')
+      },
+    })
+
+    currentDrink.orderStatus = 'forPickup'
+
+    setorders((prevState) => {
+      prevState.shift()
+      return prevState
+    })
+    if (orders.length) {
+      setCurrentDrink(orders[0])
+    }
+    else {
+      setCurrentDrink(null)
+      setIsGrabbed([])
+      setIngredients([])
+    }
+    setDisableMakeDrink(true)
+  }
+
+
+  return (
+    <Box>
+      <Stack>
+        <FormControl className={props.ingredientClassName}>
+          {ingredients}
+        </FormControl>
+        <Button disabled={disableMakeDrink} onClick={() => buildDrink()}>
+          Make Drink
+        </Button>
+      </Stack>
+    </Box>
+  )
 }
